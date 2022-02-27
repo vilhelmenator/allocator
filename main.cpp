c7549a96572899afb154c1de878f41ed9891b399
Vilhelm Páll Sævarsson <vilhelmenator@gmail.com>
2022-02-27 11:30:38 +0000

    blerg

--- main.cpp
+++ main.cpp
@@ -11,42 +11,41 @@
 
 #include "allocator.h"
 
-const int NUMBER_OF_ITEMS = 1000;
-const int NUMBER_OF_ITERATIONS = 1000000;
+const int NUMBER_OF_ITEMS = 42000;
+const int NUMBER_OF_ITERATIONS = 100;
+const int OBJECT_SIZE = (1 << 3);
 
 thread_local size_t Allocator::_thread_id = 0;
 thread_local int32_t Allocator::_thread_idx = 0;
+#define min(x, y) ((x) < (y) ? (x) : (y))
+
 int main(int argc, const char *argv[])
 {
 
     Allocator alloc;
     char *variables[NUMBER_OF_ITEMS];
-    char *variables_large[NUMBER_OF_ITERATIONS];
     auto t1 = std::chrono::high_resolution_clock::now();
     for (int j = 0; j < NUMBER_OF_ITERATIONS; j++) {
-        auto _t1 = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < NUMBER_OF_ITEMS; i++)
-            variables[i] = (char *)alloc.malloc(16);
+            variables[i] = (char *)alloc.malloc(OBJECT_SIZE);
         for (int i = 0; i < NUMBER_OF_ITEMS; i++)
             alloc.free(variables[i]);
     }
     auto t2 = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-    std::cout << "Time spent in alloc: " << duration << " milliseconds"
-              << std::endl
+    std::cout << "Time spent in alloc: " << duration << " milliseconds" << std::endl
               << std::endl;
     t1 = std::chrono::high_resolution_clock::now();
     for (int j = 0; j < NUMBER_OF_ITERATIONS; j++) {
         for (int i = 0; i < NUMBER_OF_ITEMS; i++)
-            variables[i] = (char *)malloc(16);
+            variables[i] = (char *)malloc(OBJECT_SIZE);
 
         for (int i = 0; i < NUMBER_OF_ITEMS; i++)
             free(variables[i]);
     }
     t2 = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-    std::cout << "Time spent in mi_malloc: " << duration << " milliseconds"
-              << std::endl
+    std::cout << "Time spent in mi_malloc: " << duration << " milliseconds" << std::endl
               << std::endl;
 
     return 0;
